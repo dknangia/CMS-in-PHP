bf62041f67e2c4980c0b34ee21b63bc4a761a6ba
Dknangia <deepak.nangia@trader.ca>
2022-01-14 21:58:51 -0500

    DB Connection and fetching results

---
+++ articles.php
@@ -0,0 +1,60 @@
+<?php
+
+$db_host = "localhost";
+$db_name = "cms";
+$db_user = "cms_www";
+$db_pass = "Welcome@12";
+$articles = array();
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
+
+$sql = "SELECT * FROM article ORDER BY title";
+
+$results = mysqli_query($conn, $sql);
+
+if ($results === false) {
+    echo mysqli_error($conn);
+} else {
+
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>My Blog</h1>
+    </header>
+    <main>
+        <?php if (empty($articles)) : ?>
+            <p>No records found</p>
+        <?php else : ?>
+            <ul>
+                <?php foreach ($articles as $article) : ?>
+                    <li>
+                        <article>
+                            <h2><?php echo $article['title']; ?></h2>
+                            <p><?php echo $article['content']; ?></p>
+                        </article>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </main>
+</body>
+
+</html>
