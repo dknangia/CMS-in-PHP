1fbb99d913898e57ec27ade050fa91ba269f9ef7
Dknangia <dknangia@gmail.com>
2022-01-15 22:45:30 -0500

    Moving DB setting to a configuratoin file.

---
+++ includes/database.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_host = "localhost";
+$db_name = "cms";
+$db_user = "cms_www";
+$db_pass = "Welcome@12";
+$articles = array();
+$errorMessage = "";
+
+
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
