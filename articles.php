7bc04bff934788716ccfcad7299c4f231232e688
Dknangia <dknangia@gmail.com>
2022-01-15 22:44:12 -0500

    Single article page with query string

--- articles.php
+++ articles.php
@@ -1,19 +1,8 @@
 <?php
 
-$db_host = "localhost";
-$db_name = "cms";
-$db_user = "cms_www";
-$db_pass = "Welcome@12";
-$articles = array();
+require "includes/database.php";
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
-}
-
-$sql = "SELECT * FROM article WHERE ID = 500 ORDER BY title";
+$sql = "SELECT * FROM article ORDER BY title";
 
 $results = mysqli_query($conn, $sql);
 
@@ -25,36 +14,21 @@ if ($results === false) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en"> 
+<?php require "includes/header.php" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-    <header>
-        <h1>My Blog</h1>
-    </header>
-    <main>
-        <?php if (empty($articles)) : ?>
-            <p>No records found</p>
-        <?php else : ?>
-            <ul>
-                <?php foreach ($articles as $article) : ?>
-                    <li>
-                        <article>
-                            <h2><?php echo $article['title']; ?></h2>
-                            <p><?php echo $article['content']; ?></p>
-                        </article>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-    </main>
-</body>
-
-</html>
+<?php if (empty($articles)) : ?>
+    <p>No records found</p>
+<?php else : ?>
+    <ul>
+        <?php foreach ($articles as $article) : ?>
+            <li>
+                <article>
+                    <h2><?php echo "<a href=\"\article.php?id={$article['id']}\">{$article['title']}</a>"; ?></h2>
+                    <p><?php echo $article['content']; ?></p>
+                </article>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+<?php require "includes/footer.php" ?>
