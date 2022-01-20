5b8716f0e0cf14a4bc51aa81c770c82a19e9031a
Dknangia <dknangia@gmail.com>
2022-01-20 15:23:47 -0500

    Added validation logic and error message showing

--- new-article.php
+++ new-article.php
@@ -1,41 +1,51 @@
 <?php
 
 require "includes/database.php";
-
+$errors = [];
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
+    if (!isset($_POST['ttile'])) {
+        $errors[] = "Title is required";
+    }
 
-    $sql = "INSERT INTO article (title, content, published_at)
+    if (!isset($_POST['content'])) {
+        $errors[] = "Content is required";
+    }
+
+    if (empty($errors)) {
+        $conn = getDB();
+        $sql = "INSERT INTO article (title, content, published_at)
             (?, ?, ?, ?)";
 
-    $stmt = mysqli_prepare($conn, $sql);
+        $stmt = mysqli_prepare($conn, $sql);
 
-
-
-    if ($stmt === false) {
-        echo mysqli_error($conn);
-    } else {
-
-        mysqli_stmt_bind_param($stmt, "sss", $_POST['title'], $_POST['content'], $_POST['published_at']);
-
-        if (mysqli_stmt_execute($stmt)) {
-            $id = mysqli_insert_id($conn);
-            echo "Inserted record with ID : $id";
+        if ($stmt === false) {
+            echo mysqli_error($conn);
         } else {
-            echo mysqli_stmt_error($stmt);
+
+            mysqli_stmt_bind_param($stmt, "sss", $_POST['title'], $_POST['content'], $_POST['published_at']);
+
+            if (mysqli_stmt_execute($stmt)) {
+                $id = mysqli_insert_id($conn);
+                echo "Inserted record with ID : $id";
+            } else {
+                echo mysqli_stmt_error($stmt);
+            }
         }
     }
 }
 require "includes/header.php";
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
-    var_dump($_POST);
-}
 ?>
 
 
 <h1>New article</h1>
+<?php if (!empty($errors)) : ?>
+    <ul>
+        <?php foreach ($errors as $key => $value) : ?>
+            <li><?= $value ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 <form method="post">
     <div>
         <label for="title">Title</label>
