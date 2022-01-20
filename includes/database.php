6107da2db754575fb061462a821651b4e96c9627
Dknangia <dknangia@gmail.com>
2022-01-20 15:24:24 -0500

    Encapsulated the DB connectivity code into a function

--- includes/database.php
+++ includes/database.php
@@ -1,17 +1,29 @@
 <?php
+/**
+ * Get the db connection
+ * 
+ * @param : none
+ * 
+ * @return : Database connection 
+ */
+function getDB()
+{
 
-$db_host = "localhost";
-$db_name = "cms";
-$db_user = "cms_www";
-$db_pass = "Welcome@12";
-$articles = array();
-$errorMessage = "";
+    $db_host = "localhost";
+    $db_name = "cms";
+    $db_user = "cms_www";
+    $db_pass = "Welcome@12";
+    $articles = array();
+    $errorMessage = "";
 
 
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
+    if (mysqli_connect_error()) {
+        echo mysqli_connect_error();
+        exit;
+    }
+
+    return $conn;
 }
