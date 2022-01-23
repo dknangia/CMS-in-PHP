b663e3d6103276717013dd5f7da1b2f1ad859149
Dknangia <dknangia@gmail.com>
2022-01-23 08:20:19 -0500

    Static login implementation

--- login.php
+++ login.php
@@ -1,4 +1,38 @@
-<?php 
-session_start(); 
+<?php
+session_start();
+require "includes/url.php";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    
+    if ($_POST['username'] == "dknangia" && $_POST['password'] === "1234") {
+       $_SESSION['is_logged_in'] = true;
+        redirect("/articles.php");
+        exit;
+    } else {
+        
+        $error = "Incorrect login";
+    }
+}
 
-$_SESSION['is_logged_in'] = true;
+?>
+
+<?php include "includes/header.php"; ?>
+
+<h2>Login</h2>
+<?php if (!empty($error)) : ?>
+    <p><?= $error ?></p>
+<?php endif; ?>
+<form method="POST">
+    <p>
+        <lable for="username">User name</lable>
+        <input type="text" name="username">
+    </p>
+    <p>
+        <lable for="password">Password</lable>
+        <input type="text" name="password">
+    </p>
+    <p>
+        <button>Login</button>
+    </p>
+</form>
+
+<?php include "includes/footer.php" ?>
