8f8a701cd2a3f18fb48954dde6397916f13c444f
Dknangia <dknangia@gmail.com>
2022-01-22 21:33:56 -0500

    File for redirect related fucntionalites

---
+++ includes/url.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * Redirect the request to the specified path
+ * 
+ * @param string $path Path were request needs to be redirected
+ * 
+ * 
+ */
+function redirect($path){
+    $protocol = '';
+
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+        $protocol = 'https';
+    } else {
+        $protocol = 'http';
+    }
+
+    //Redirect the user to another page
+    header("Location: $protocol://" . $_SERVER['HTTP_HOST'] . $path);
+    exit;
+}
