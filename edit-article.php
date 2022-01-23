9aceef5f967e3cf4649b149d531623683e6324c9
Dknangia <dknangia@gmail.com>
2022-01-22 19:06:35 -0500

    Introduced update article logic.

--- edit-article.php
+++ edit-article.php
@@ -27,6 +27,7 @@ if (isset($_GET["id"]) && is_numeric($_GET["id"])) {
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
+    $id = htmlspecialchars($_GET['id']);
     $title = htmlspecialchars($_POST['title']);
     $content = htmlspecialchars($_POST['content']);
     $published_at = $_POST['published_at'];
@@ -34,10 +35,42 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $errors = validateArticle($title, $content, $published_at);
 
     if (empty($errors)) {
-        //Update 
+        $conn = getDB();
+        $sql = "UPDATE  article
+                SET title = ?
+                , content  = ?
+                , published_at = ?
+                WHERE id = ?";
+
+        $stmt = mysqli_prepare($conn, $sql);
+
+        if ($stmt === false) {
+            echo mysqli_error($conn);
+        } else {
+
+            if (empty($published_at)) {
+                $published_at = null;
+            }
+
+            mysqli_stmt_bind_param($stmt, "sssi", $title, $content, $published_at, $id);
+
+            if (mysqli_stmt_execute($stmt)) {
+               
+                $protocol = '';
+                if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+                    $protocol = 'https';
+                } else {
+                    $protocol = 'http';
+                }
+
+                //Redirect the user to article details page
+                header("Location: $protocol://" . $_SERVER['HTTP_HOST'] . "/article.php?id=$id");
+                exit;
+            } else {
+                echo mysqli_stmt_error($stmt);
+            }
+        }
     }
-
-
 }
 
 require "includes/header.php";
