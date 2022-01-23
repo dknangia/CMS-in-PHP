eaf739bbc9e1d59b1a37172e7802d55473aa0868
Dknangia <dknangia@gmail.com>
2022-01-23 06:55:02 -0500

    Optimized the SELECT query by adding $columns parameter

--- includes/article.php
+++ includes/article.php
@@ -9,10 +9,10 @@
  * 
  */
 
-function getArticle($conn, $id)
+function getArticle($conn, $id, $columns = "*")
 {
 
-    $sql = "SELECT * FROM article WHERE id = ?";
+    $sql = "SELECT $columns FROM article WHERE id = ?";
 
     $stmt = mysqli_prepare($conn, $sql);
 
