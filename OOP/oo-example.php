1e00dc4af53a87c2006c3434885222b98c369005
Dknangia <dknangia@gmail.com>
2022-01-25 22:13:09 -0500

    Properties and dynamic property of a class

--- OOP/oo-example.php
+++ OOP/oo-example.php
@@ -2,5 +2,11 @@
 require "Item.php";
 
 $myObject = new Item();
+$myObject->name  = "Item name"; 
+$myObject->description; 
+
+//Dynamic property 
+$myObject->price = 0.25; 
+
 
 var_dump($myObject);
