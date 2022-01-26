b0c7cd0058e4497c9ede8514f06bda747b77c5b9
Dknangia <dknangia@gmail.com>
2022-01-25 22:44:54 -0500

    Constant variables and using it.

--- OOP/Item.php
+++ OOP/Item.php
@@ -2,62 +2,7 @@
 
 class Item
 {
-   private $name;
-
-   private $description;
-
-   public static $count = 0;
-
-   public function __construct()
-   {
-      static::$count++;
-   }
-   /**
-    * Get the value of name
-    */
-   public function getName()
-   {
-      return $this->name;
-   }
-
-   /**
-    * Set the value of name
-    *
-    * @return  self
-    */
-   public function setName($name)
-   {
-      $this->name = $name;
-
-      return $this;
-   }
-
-   /**
-    * Get the value of description
-    */
-   public function getDescription()
-   {
-      return $this->description;
-   }
-
-   /**
-    * Set the value of description
-    *
-    * @return  self
-    */
-   public function setDescription($description)
-   {
-      $this->description = $description;
-
-      return $this;
-   }
-
-   /**
-    * Static method 
-
-    */
-   public static function readingCountValue()
-   {
-      return "Static variable value " . static::$count;
-   }
+   //Constant are static values, but we don't need to use STATIC keyword
+   public const MAXIMUM = "123"; 
+   
 }
--- OOP/oo-example.php
+++ OOP/oo-example.php
@@ -2,18 +2,4 @@
 require "Item.php";
 
 
-var_dump(Item::$count);
-
-$myObject = new Item();
-
-$myObject->setName("Product X123"); 
-echo $myObject->getName();
-
-var_dump(Item::$count);
-
-$secondObject = new Item();
-
-var_dump(Item::$count);
-
-echo "<br>"; 
-echo Item::readingCountValue();
+echo Item::MAXIMUM; 
