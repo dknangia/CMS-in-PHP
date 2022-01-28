1ad8b8d57064405c2eeabac4aab0f6e838d96b78
Dknangia <dknangia@gmail.com>
2022-01-28 18:15:13 -0500

    Book keyword to extend the existing class and override method.

---
+++ OOP/book.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+class Book extends Item
+{
+    public $author;
+
+    public function getListingDescription()
+    {
+        return parent::getListingDescription() . " by " . $this->author;
+    }
+}
