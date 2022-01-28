17d95ba9554ace33d56d3703141d9deefb489cf4
Dknangia <dknangia@gmail.com>
2022-01-28 18:55:45 -0500

    Introduced ERROR configuration in PDO object

--- classes/Database.php
+++ classes/Database.php
@@ -1,26 +1,34 @@
-<?php 
+<?php
+
 /**
  * Database 
  * 
  * A connection to the database server
  */
-class Database{
+class Database
+{
 
     /**
      * Get the database connection 
      * 
      * @return PDO object connection to the database server 
      */
-    public function getConnection(){
+    public function getConnection()
+    {
         $db_host = "localhost";
         $db_name = "cms";
         $db_user = "cms_www";
         $db_pass = "Welcome@12";
-        
-        $dsn="mysql:host=" . $db_host . ";dbname=". $db_name. ";charset=utf8";
 
-        return $conn = new PDO($dsn, $db_user, $db_pass); 
+        $dsn = "mysql:host=" . $db_host . ";dbname=" . $db_name . ";charset=utf8";
 
-    
+        try {
+            $conn = new PDO($dsn, $db_user, $db_pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+            exit;
+        }
     }
 }
