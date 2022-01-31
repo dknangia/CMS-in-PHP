521b05c2648360dbeafd880974ecf7ce83e0ab5e
Dknangia <dknangia@gmail.com>
2022-01-31 15:36:32 -0500

    Updated code to use Article class method to update, and used parameterized query.

--- classes/Article.php
+++ classes/Article.php
@@ -6,9 +6,9 @@
  */
 class Article
 {
-    public $id; 
-    public $title; 
-    public $content; 
+    public $id;
+    public $title;
+    public $content;
     public $published_at;
 
     public static function getAllArticles($conn)
@@ -20,28 +20,53 @@ class Article
     }
 
     /**
- * 
- * @param Object $conn Connection to the database
- * @param Object $id ID of the article to find
- * 
- * @return mixed An object of this class.
- * 
- */
+     * 
+     * @param Object $conn Connection to the database
+     * @param Object $id ID of the article to find
+     * 
+     * @return mixed An object of this class.
+     * 
+     */
 
-public static function getArticleById($conn, $id, $columns = "*")
-{
+    public static function getArticleById($conn, $id, $columns = "*")
+    {
 
-    $sql = "SELECT $columns FROM article WHERE id = :id";
+        $sql = "SELECT $columns FROM article WHERE id = :id";
 
-    $stmt = $conn->prepare($sql);
+        $stmt = $conn->prepare($sql);
 
-    $stmt->bindValue(':id', $id,  PDO::PARAM_INT);
+        $stmt->bindValue(':id', $id,  PDO::PARAM_INT);
 
-    $stmt->setFetchMode(PDO::FETCH_CLASS, 'Article');
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Article');
 
-    if ($stmt->execute()) {
-        return $stmt->fetch();
+        if ($stmt->execute()) {
+            return $stmt->fetch();
+        }
     }
-}
 
+
+    public function updateArticleByID($conn)
+    {
+        $sql = "UPDATE article 
+                SET title = :title, 
+                    content = :content, 
+                    published_at = :published_at
+                WHERE 
+                    Id = :id";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
+        $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+        $stmt->bindValue(':content', $this->content, PDO::PARAM_STR);
+
+        //Complex statement 
+        $stmt->bindValue(
+            ':published_at',
+            $this->published_at == '' ? null : $this->published_at,
+            $this->published_at == '' ? PDO::PARAM_NULL : PDO::PARAM_STR
+        );
+
+
+        return $stmt->execute();
+    }
 }
--- edit-article.php
+++ edit-article.php
@@ -12,16 +12,7 @@ if (isset($_GET["id"]) && is_numeric($_GET["id"])) {
     $id = $_GET['id'];
     $article = Article::getArticleById($conn, $id);
 
-    $title = '';
-    $content = '';
-    $published_at = '';
-
-    if ($article !== null) {
-
-        $title = $article['title'];
-        $content = $article['content'];
-        $published_at = $article['published_at'];
-    } else {
+    if (!$article) {
         die("No article found with this id");
     }
 } else {
@@ -30,40 +21,16 @@ if (isset($_GET["id"]) && is_numeric($_GET["id"])) {
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-    $id = htmlspecialchars($_GET['id']);
-    $title = htmlspecialchars($_POST['title']);
-    $content = htmlspecialchars($_POST['content']);
-    $published_at = $_POST['published_at'];
+    $article->id = htmlspecialchars($_GET['id']);
+    $article->title = htmlspecialchars($_POST['title']);
+    $article->content = htmlspecialchars($_POST['content']);
+    $article->published_at = $_POST['published_at'];
 
-    $errors = validateArticle($title, $content, $published_at);
+    $errors = validateArticle($article->title, $article->content, $article->published_at);
 
     if (empty($errors)) {
-        $conn = getDB();
-        $sql = "UPDATE  article
-                SET title = ?
-                , content  = ?
-                , published_at = ?
-                WHERE id = ?";
-
-        $stmt = mysqli_prepare($conn, $sql);
-
-        if ($stmt === false) {
-            echo mysqli_error($conn);
-        } else {
-
-            if (empty($published_at)) {
-                $published_at = null;
-            }
-
-            mysqli_stmt_bind_param($stmt, "sssi", $title, $content, $published_at, $id);
-
-            if (mysqli_stmt_execute($stmt)) {             
-
-                redirect("/article.php?id=$id");
-                exit;
-            } else {
-                echo mysqli_stmt_error($stmt);
-            }
+        if ($article->updateArticleByID($conn)) {
+            redirect("/article.php?id=$article->id");         
         }
     }
 }
