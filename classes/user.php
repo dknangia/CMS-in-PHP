6e7de50ef46608e28630d2ee2b5ea598a24a6f4d
Dknangia <dknangia@gmail.com>
2022-02-02 00:19:33 -0500

    Update code to use DB for user information

--- classes/user.php
+++ classes/user.php
@@ -3,6 +3,10 @@
 class User
 {
 
+    public $id;
+    public $usernmae;
+    public $password;
+
     /**
      * User class for authentication functions
      * 
@@ -11,9 +15,21 @@ class User
      * 
      * @return boolean true on sucess and false on failure 
      */
-    public static function authenticate($username, $password)
+    public static function authenticate($conn, $username, $password)
     {
-        return $username == 'dnangia' && $password == "1234";
+        $sql = "SELECT * 
+                FROM user
+                WHERE username = :username";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(":username", $username, PDO::PARAM_STR);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+
+        $stmt->execute();
+
+        if ($user = $stmt->fetch())
+            return $user->password == $password;
     }
 
     public static function logout()
