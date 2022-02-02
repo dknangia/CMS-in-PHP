761dbd3ce30bac36498fb74acdaaaa86a073163d
Dknangia <dknangia@gmail.com>
2022-02-01 23:23:10 -0500

    Added new Insert article method

--- classes/Article.php
+++ classes/Article.php
@@ -79,20 +79,56 @@ class Article
     /**
      * Delete article based on id; 
      * 
-     * @return bool 
+     * @return bool return true on successfull execution of the command, and return false on failure. 
      */
     public function deteleArticleById($conn)
     {
-      
-            $sql = "DELETE FROM article 
+
+        $sql = "DELETE FROM article 
                     WHERE 
                     Id = :id";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(":id", $this->id, PDO::PARAM_INT);
+
+        return ($stmt->execute());
+    }
+
+
+    /**
+     * Create new article in database 
+     * 
+     * @return boolean return true on success and false on failure
+     */
+
+    public function insertNewArticle($conn)
+    {
+        if ($this->validate()) {
+            $sql = "INSERT INTO article (title, content, published_at)
+                VALUES (:title, 
+                        :content, 
+                        :published_at)";
+
             $stmt = $conn->prepare($sql);
 
-            $stmt->bindValue(":id", $this->id, PDO::PARAM_INT);
 
-            return ($stmt->execute());
-       
+            $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+            $stmt->bindValue(':content', $this->content, PDO::PARAM_STR);
+
+            $stmt->bindValue(
+                ':published_at',
+                $this->published_at == '' ? null : $this->published_at,
+                $this->published_at == '' ? PDO::PARAM_NULL : PDO::PARAM_STR
+            );
+
+            if ($stmt->execute()) {
+                $this->id =  $conn->lastInsertId();
+                return true;
+            } else {
+                false;
+            }
+        }
+        return false;
     }
 
 
