87e4fa8245cf6bc1fbe8196c55fc82565d184f96
Dknangia <dknangia@gmail.com>
2022-02-06 15:48:12 -0500

    Use Auth class login logout method.

--- login.php
+++ login.php
@@ -10,8 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $conn = require "includes/db.php";
 
     if (USER::authenticate($conn, $_POST['username'], $_POST['password'])) {
-        session_regenerate_id(true);
-        $_SESSION['is_logged_in'] = true;
+        Auth::Login();
         Url::redirect("/articles.php");
         exit;
     } else {
