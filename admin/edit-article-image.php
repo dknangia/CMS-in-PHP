001c5457f97bf7929af8902a2f87f47bc51e7b4a
Dknangia <dknangia@gmail.com>
2022-02-09 12:27:52 -0500

    File name sanitization

--- admin/edit-article-image.php
+++ admin/edit-article-image.php
@@ -51,7 +51,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             throw new Exception(("Invalid file type"));
         }
 
-        $destination = "../uploads/" . $_FILES['file']['name'];
+        $pathinfo = pathinfo($_FILES['file']['name']);
+
+        $base = $pathinfo['filename'];
+
+        $base = preg_replace('/[^a-zA-Z0-9_-]/', '_', $base);
+
+        $filename = $base . "." . $pathinfo['extension'];
+
+        $destination = "../uploads/" . $filename;
 
         if (move_uploaded_file($_FILES['file']['tmp_name'], $destination)) {
             echo "file uploaded successfully";
