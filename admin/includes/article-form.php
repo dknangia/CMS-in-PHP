2c0feaa7e96fe214a7f74424008c37052e58feaf
Dknangia <dknangia@gmail.com>
2022-02-12 23:12:41 -0500

    Updated for to show categories

--- admin/includes/article-form.php
+++ admin/includes/article-form.php
@@ -20,5 +20,14 @@
         <label for="published_at">Publication date and time</label>
         <input type="datetime" id="published_at" name="published_at" placeholder="Published date" value="<?= htmlspecialchars($article->published_at); ?>">
     </div>
+    <fieldset>
+        <legend>Categories</legend>
+        <?php foreach ($categories as $category) : ?>
+            <div>
+                <input type="checkbox" name="category[]" id="<?= htmlspecialchars($category['id']) ?>" <?php if (in_array($category['id'], $category_id)) : ?> checked <?php endif; ?> value="<?= $category['id'] ?>">
+                <label for="<?= htmlspecialchars($category['id']) ?>"><?= htmlspecialchars($category['name']) ?></label>
+            </div>
+        <?php endforeach; ?>
+    </fieldset>
     <button>Save</button>
 </form>
