7b469727c7c6e380c0c2957bea420236cfe9b778
Dknangia <dknangia@gmail.com>
2022-02-13 21:34:07 -0500

    Updated comments

--- classes/Category.php
+++ classes/Category.php
@@ -3,6 +3,14 @@ class Category{
     public $id;
     public $category_name;
 
+
+    /**
+     * Return all the categories from DB
+     * 
+     * @param object $conn DB connection
+     * 
+     * @return array result set in Category object array 
+     */
     public static function getAll($conn)
     {
         $sql = "SELECT * FROM category ORDER BY name";
